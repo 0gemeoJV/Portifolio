7bdc4a9a7fcf8c8ce248747193a42e76199e3e01
João Vítor Matos dos Santos <jvmatossantos00@gmail.com>
2026-06-11 11:27:34 -0300

    fix: Ajuste na transição do título do menu ao rolar a página

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', function () {
         menuBtn.classList.toggle('active');
     });
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    
+    // Se a rolagem passar de 50px, adiciona a classe; se voltar ao topo, remove.
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
